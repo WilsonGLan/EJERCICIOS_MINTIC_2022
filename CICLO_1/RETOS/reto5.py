7c327f088b8bb9669f7722cd9cdd63ec6a0e5038
Wilson Gutiérrez L <wilsongutierrezl@gmail.com>
2022-06-19 23:06:59 -0500

    Se completa el código de la plantilla en el archivo reto5.py que hace parte del reto 5 y que cumple con las condiciones solicitadas

--- CICLO_1/RETOS/reto5.py
+++ CICLO_1/RETOS/reto5.py
@@ -13,7 +13,7 @@ el sistema debe leer la información del archivo data.csv, que contiene los sigu
 
 '''
 
-ruta = "D:/00_CURSOS/MINTIC/PROGRAMACION_BASICA/EJERCICIOS_MINTIC_2022/CICLO_1/RETOS/data.csv"
+ruta = "data.csv"
 
 def leerArchivo():
         with open(ruta, 'r') as datafile:
@@ -28,3 +28,62 @@ def leerArchivo():
                         matriz.append(fila)
                         linea = datafile.readline()
         return matriz, sucursales, encabezado
+
+def main():
+    # Arreglos de datos que se obtienen de la funcion
+    pacientes, centrales, columnas= leerArchivo()
+
+    # Obtiene los indices de las columnas utilizadas en el proceso
+    i_gender = columnas.index('gender')
+    i_branch = columnas.index('id_branch')
+    i_ps = columnas.index('systolic_pressure')
+    i_pd = columnas.index('diastolic_pressure')
+    i_qm = columnas.index('medicine_quantity')
+    hombres = 0
+    mujeres = 0
+    len_pacientes = len(pacientes)
+    cant_med = 0
+
+    # Capturar la central ingresada como Entrada
+    input_central= int(input())
+    #
+    for i in range(len_pacientes):
+        if int(pacientes[i][i_branch]) == input_central:
+            entrega= False
+            sis, dia = int(pacientes[i][i_ps]), int(pacientes[i][i_pd])
+            if sis < 91 and dia < 63:
+                entrega= True
+            elif 162 <= sis < 188 and 105 <= dia < 119:
+                entrega= True
+            elif 188 <= sis < 201 and 119 <= dia < 126:
+                entrega= True
+            elif 201 <= sis < 214 and 126 <= dia < 146:
+                entrega= True
+            elif sis >= 214 and dia >= 146:
+                entrega= True
+            elif sis >= 152 and dia < 77:
+                entrega= True
+            else:
+                entrega= False
+
+            if entrega:
+                if pacientes[i][i_gender]=='m':
+                    hombres += 1
+                else :
+                    mujeres += 1
+
+                # Acumula Cantidad Total de Medicamentos solicitados por el Cliente
+                cant_med += int(pacientes[i][i_qm])
+
+    # Salidas esperadas
+    print(f"{input_central} {centrales[input_central-1]}")
+    print("scheduled patients")
+    print(f'male {hombres}')
+    print(f'female {mujeres}')
+    print(f'total {hombres+mujeres}')
+    print('scheduled medicine quantity')
+    print('mean {:.2f}'.format(cant_med/(hombres+mujeres)))
+    print(f'total {cant_med}')
+
+if __name__=='__main__':
+    main()
