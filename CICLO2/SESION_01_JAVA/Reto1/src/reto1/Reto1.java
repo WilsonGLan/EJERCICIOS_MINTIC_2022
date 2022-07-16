4b4a5bc903e43bfd2805afb801bc7c228cb4fde4
Wilson Gutierrez <wilsongutierrezl@gmail.com>
2022-07-16 14:21:38 -0500

    Finalizando y entregando el reto1 sin errores

--- CICLO2/SESION_01_JAVA/Reto1/src/reto1/Reto1.java
+++ CICLO2/SESION_01_JAVA/Reto1/src/reto1/Reto1.java
@@ -1,19 +1,72 @@
 package reto1;
-
 import java.util.Scanner;
 
-/**
- * @author Wilson Gutierrez
- */
-public class Reto1 {
+public class reto1 {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
-        int tamano = entrada.nextInt();
+        
+        int total = 0;
+        float promedio ;
+        long elemento , mayor = 0, menor = 0;
+        String pantalla;
+        
+        int tamano = Integer.parseInt(entrada.nextLine());  
         long porcentajes [] = new long [tamano];
         for (int i = 0; i < tamano; i++){
-            porcentajes[i] = entrada.nextInt();
+            porcentajes[i] = Long.parseLong(entrada.nextLine()); 
         }
+        
+        for (int i = 0; i < tamano; i++){
+            total += porcentajes[i];
+        }
+        
+        promedio = total / tamano;            
+        pantalla = validar(promedio);        
+        System.out.println(pantalla);
+        
+        elemento = porcentajes[0];
+        for (int i = 1; i < tamano; i++){
+            if (elemento <= porcentajes[i]) {
+                elemento = porcentajes[i];
+                mayor = elemento;
+            }else{
+                mayor = elemento;
+            }
+        }
+        
+        pantalla = validar(mayor);        
+        System.out.println(pantalla);
+        
+        
+        elemento = porcentajes[0];
+        for (int i = 1; i < tamano; i++){
+            if (elemento <= porcentajes[i]) {
+                menor = elemento;
+            }else{
+                elemento = porcentajes[i];
+                menor = elemento;
+            }
+        }
+                
+        pantalla = validar(menor);        
+        System.out.println(pantalla);
+        
     }
     
+            static String validar(float valor){
+        String nivelRiesgo = "";
+        if (valor > 80 && valor <= 100){
+            nivelRiesgo = "INVIABLE SANITARIAMENTE";
+        }else if (valor > 35 && valor <= 80) {
+            nivelRiesgo = "ALTO";
+        }else if (valor > 14 && valor <= 35) {
+            nivelRiesgo = "MEDIO";
+        }else if (valor > 5 && valor <= 14) {
+            nivelRiesgo = "BAJO";
+        }else if (valor > 0 && valor <= 5) {
+            nivelRiesgo = "SIN RIESGO";
+        }
+        return nivelRiesgo;
+    }
     
 }
