204e501b0a5ea3aee025ba373c209d4a8832e775
Wilson Gutierrez <wilsongutierrezl@gmail.com>
2022-07-24 00:22:55 -0500

    Se agregan llamadas a los metodos faltantes

--- CICLO2/SESION_01_JAVA/Reto2/src/reto2/reto2.java
+++ CICLO2/SESION_01_JAVA/Reto2/src/reto2/reto2.java
@@ -5,24 +5,28 @@ public class reto2 {
     public static void main(String[] args) {        
         Scanner  leerCantidad=new Scanner(System.in);
         int numCuerpos = leerCantidad.nextInt();
-        String nombreCA [] = new String[numCuerpos];
-        String munCA [] = new String[numCuerpos];
+        String nombreCA [] = new String[numCuerpos];        
         int  idCA [] = new int[numCuerpos];
-        double clasificacionIRCA [] = new double[numCuerpos];
+        double clasificacionIRCA [] = new double[numCuerpos];        
         
         for (int i = 0; i < numCuerpos; i++) {
             Scanner  leerDatos=new Scanner(System.in);
             String datosNR = leerDatos.nextLine();
             String [] listaDatos = datosNR.split(" ");
             nombreCA[i] = listaDatos[0];
-            idCA[i] = Integer.parseInt(listaDatos[1]);
-            munCA[i] = listaDatos[2];
+            idCA[i] = Integer.parseInt(listaDatos[1]);            
             clasificacionIRCA[i] = Double.parseDouble(listaDatos[3]);
         }
         
-        //CuerpoDeAgua validarCA = new CuerpoDeAgua(nombreCA, idCA, munCA, clasificacionIRCA);
-
+        CuerpoDeAgua validarCA = new CuerpoDeAgua(nombreCA, idCA, clasificacionIRCA);
         
-    }
+        for (int i = 0; i < numCuerpos; i++) {
+            System.out.println(validarCA.nivel(i));
+        }
+        System.out.println(validarCA.cantMBSR());
+        
+        validarCA.nombreMedio();
+        validarCA.clasificaBaja();
     
+    }
 }
