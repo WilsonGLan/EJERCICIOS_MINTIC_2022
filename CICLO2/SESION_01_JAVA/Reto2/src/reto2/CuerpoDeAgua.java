f0964b47ea7f02f80a223ab8ab6519c7dc6cd250
Wilson Gutierrez <wilsongutierrezl@gmail.com>
2022-07-23 22:12:00 -0500

    Calcular el nivel de riesgo

--- CICLO2/SESION_01_JAVA/Reto2/src/reto2/CuerpoDeAgua.java
+++ CICLO2/SESION_01_JAVA/Reto2/src/reto2/CuerpoDeAgua.java
@@ -2,52 +2,34 @@
 package reto2;
 
 public class CuerpoDeAgua {
-    private String nombre;
-    private int id;
-    private String municipio;
-    private double irca;
+    private String [] nombre;
+    private int  [] id;
+    private String  [] municipio;
+    private double  [] irca;
+    private int tamano;
 
-    public CuerpoDeAgua(String nombre, int id, String municipio, double irca) {
+    public CuerpoDeAgua(String[] nombre, int[] id, String[] municipio, double[] irca, int tamano) {
         this.nombre = nombre;
         this.id = id;
         this.municipio = municipio;
         this.irca = irca;
+        this.tamano = tamano;
     }
 
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getMunicipio() {
-        return municipio;
-    }
-
-    public void setMunicipio(String municipio) {
-        this.municipio = municipio;
-    }
-
-    public double getIrca() {
-        return irca;
-    }
-
-    public void setIrca(double irca) {
-        this.irca = irca;
-    }
-    
-    public void nivel(){
-        
+    public void nivel(int posicion){
+        if (irca[posicion ] > 80 && irca[posicion ] <= 100) {
+            System.out.println("INVIABLE SANITARIAMENTE");
+        }else if (irca[posicion ] > 35 && irca[posicion ] <= 80) {
+            System.out.println("ALTO");
+        }else if (irca[posicion ] > 14 && irca[posicion ] <= 35) {
+            System.out.println("MEDIO");
+        }else if (irca[posicion ] > 5 && irca[posicion ] <= 14) {
+            System.out.println("BAJO");
+        }else if (irca[posicion ] >=0 && irca[posicion ] <=5) {
+            System.out.println("SIN RIESGO");
+        }else {
+            System.out.println("NA");
+        }
     }
     
 }
