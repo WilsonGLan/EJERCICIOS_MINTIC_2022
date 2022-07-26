77a97b19c6f9c274a723d21bfcdc2f8246335886
Wilson Gutierrez <wilsongutierrezl@gmail.com>
2022-07-25 21:57:15 -0500

    Quitando una declaración de escanner innecesaria

--- CICLO2/SESION_01_JAVA/Reto2/src/reto2/reto2.java
+++ CICLO2/SESION_01_JAVA/Reto2/src/reto2/reto2.java
@@ -3,21 +3,21 @@ import java.util.Scanner;
 
 public class reto2 {
     public static void main(String[] args) {        
-        Scanner  leerCantidad=new Scanner(System.in);
-        int numCuerpos = leerCantidad.nextInt();
+        Scanner  leer=new Scanner(System.in);
+        int numCuerpos = Integer.parseInt(leer.nextLine());
         String nombreCA [] = new String[numCuerpos];        
         int  idCA [] = new int[numCuerpos];
-        double clasificacionIRCA [] = new double[numCuerpos];        
-        
-        for (int i = 0; i < numCuerpos; i++) {
-            Scanner  leerDatos=new Scanner(System.in);
-            String datosNR = leerDatos.nextLine();
+        double clasificacionIRCA [] = new double[numCuerpos];   
+        String datosNR;
+
+        for (int i = 0; i < numCuerpos; i++) {                        
+            datosNR = leer.nextLine();
             String [] listaDatos = datosNR.split(" ");
             nombreCA[i] = listaDatos[0];
             idCA[i] = Integer.parseInt(listaDatos[1]);            
             clasificacionIRCA[i] = Double.parseDouble(listaDatos[3]);
         }
-        
+         
         CuerpoDeAgua validarCA = new CuerpoDeAgua(nombreCA, idCA, clasificacionIRCA);
         
         for (int i = 0; i < numCuerpos; i++) {
